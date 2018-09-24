a9f85356bd6b8a19854a8c3d775083c11f24ce41
Fraser Love <35693813+FraserLove@users.noreply.github.com>
2018-09-24 22:34:00 +0100

    Create random_binary_file_gen.py

---
+++ tools/random_binary_file_gen.py
@@ -0,0 +1,14 @@
+"""
+Developed by Fraser Love
+"""
+import os
+import random
+
+binary = ["0","1"]
+out = []
+f = open("binary" +".txt","w+")
+for a in range(0,100000000):      #Number of char in lines
+    out.append(random.choice(binary[random.randint(0,1)]))
+f.write("".join(out))
+f.close()
+print("DONE")
