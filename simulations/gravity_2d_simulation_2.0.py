2e4613fd17c79689f416719e3bfacc7fe3a60f83
Fraser Love <35693813+FraserLove@users.noreply.github.com>
2018-10-17 22:54:55 +0200

    Update gravity_2d_simulation_2.0.py

--- simulations/gravity_2d_simulation_2.0.py
+++ simulations/gravity_2d_simulation_2.0.py
@@ -44,7 +44,7 @@ initial_objects = 200   # Number of initial objects in the simulation
 object_list = []
 G = 6.67408e-11         # Gravitational constant
 time_step = 10000       # How much time has passed between every calculation - (lower more accurate)
-frame_rate = 60
+frame_rate = 30         # time passed in sim = current time * time_step * frame_rate
 collision_radius = 6    # Distance from object where collision occurs
 start_time = time.time()
 
@@ -69,10 +69,11 @@ def add_force(object):
     x_acc = y_acc = 0.0
     for other in object_list:
         if other != object and object.x_pos - other.x_pos != 0:
-            r = sqrt((object.x_pos - other.x_pos)**2 + (object.y_pos - other.y_pos)**2)
-            fg = (G * other.mass)/ r**3
-            x_acc += fg * (other.x_pos - object.x_pos)
-            y_acc += fg * (other.y_pos - object.y_pos)
+            r = ((other.x_pos - object.x_pos)**2) + ((other.y_pos - object.y_pos)**2)
+            fg = G * object.mass * other.mass / r
+            angle = math.atan2((other.y_pos - object.y_pos), (other.x_pos - object.x_pos))
+            x_acc += fg * math.cos(angle) / object.mass
+            y_acc += fg * math.sin(angle) / object.mass
     return x_acc, y_acc
 
 def update_pos(object_list):
@@ -103,7 +104,7 @@ def main():
     setup(object_list)
     step = 1
     while True:
-        pygame.display.set_caption('Gravity Simulation: Step {}  SimulationTime(s): {} RealTime: {}  Objects: {}'.format(str(step), time_step * math.floor(time.time() - start_time), round(time.time() - start_time,2), len(object_list)))
+        pygame.display.set_caption('Gravity Simulation: Step {}  SimulationTime(s): {} RealTime: {}  Objects: {}'.format(str(step), round(round(time.time() - start_time,2)* time_step * frame_rate,2), round(time.time() - start_time,2), len(object_list)))
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit(); sys.exit();
