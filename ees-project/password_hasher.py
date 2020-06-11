9591a5b31c3d8db4bbc1631c7caf3563e772ac29
Fraser Love <me@fraser.love>
2020-06-11 17:42:00 +0100

    Major changes made to repositiory layout and archived redundant scripts

--- ees-project/password_hasher.py
+++
@@ -1,5 +0,0 @@
-from passlib.hash import pbkdf2_sha256
-
-string = input("What do you want to hash: ")
-hash = pbkdf2_sha256.hash(string)
-print(hash)
--- schoolwork/higher/string_to_ascii_converter.py
+++
@@ -1,4 +0,0 @@
-name_list = list(input("What is your name: "))
-for char in name_list:
-    name_list[name_list.index(char)] = ord(char)
-    print('{} : {}'.format(char, name_list[name_list.index(int(ord(char)))]))
