fd489f995d6b9cca6740e7ed6021874b533cffb9
fraserlove <me@fraser.love>
2021-07-05 18:35:27 +0100

    Updated sorting_visualiser.py

--- sorting-algorithms-and-visualisers/sorting-visualiser/sorting_visualiser.py
+++ sorting-algorithms-and-visualisers/sorting-visualiser/sorting_visualiser.py
@@ -1,31 +1,28 @@
 """
 Sorting Algorithm Visualiser
-
 Created 01/08/19
 Developed by Fraser Love
-
 Visualiser to be used with sorting algorithms provided in sorting_algorithms.py. There 12
 algorithms are provided including: selection sort, bubble sort, insertion sort, shell sort
 radix sort, cocktail sort, gnome sort, merge sort, quick sort, heap sort, bitonic sort and
 bucket sort. One note for the visualisation of merge sort is that because is splits and then
 combines the arrays it was difficult to fully visualise. Instead the current arrays being
 merged are shown.
-
 Usage - After typing in the name of this script enter the name of the sorting algorithm you
 want to use in CamelCase with no spaces.
-
 Note - This visualiser only for understanding how sorting algorithms work
 and should not be used to compare time complexities as most of the time
 spent by the program is drawing the array on screen and not on sorting
-If you want to see a direct comarison between algorithm in terms of their
+If you want to see a direct comparison between algorithm in terms of their
 scaling and time complexities go download my sorting algorithm comparison program.
 """
 
 import sorting_algorithms, time, os, sys
+
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 import pygame
 
-dimensions = (1024, 512) # The y value should be equal to the array length
+dimensions = (2048, 1024)  # The y value should be equal to the array length
 algorithms = {"SelectionSort": sorting_algorithms.SelectionSort(), \
               "BubbleSort": sorting_algorithms.BubbleSort(), \
               "InsertionSort": sorting_algorithms.InsertionSort(), \
@@ -48,7 +45,8 @@ if len(sys.argv) > 1:
 # Pygame Initialisation
 pygame.init()
 display = pygame.display.set_mode((dimensions[0], dimensions[1]))
-display.fill(pygame.Color("white"))
+display.fill(pygame.Color("black"))
+
 
 # Plays a sound if you are on Ubuntu/Debian
 # First install sox with: sudo apt install sox
@@ -56,33 +54,41 @@ display.fill(pygame.Color("white"))
 def play_sound(swap):
     duration = 0.01
     freq = 20
-    os.system('play -nq -t alsa synth {} sine {}'.format(duration, freq+swap*10))
+    os.system('play -nq -t alsa synth {} sine {}'.format(duration, freq + swap * 10))
+
 
 def check_events():
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            pygame.quit(); sys.exit();
+            pygame.quit();
+            sys.exit();
+
 
 def update(algorithm, swap1=None, swap2=None, display=display):
-    display.fill(pygame.Color("white"))
-    pygame.display.set_caption("Sorting Visiualiser     Algorithm: {}     Time: {:.3f}      Status: Sorting".format(algorithm.name, time.time() - algorithm.start_time))
-    k = int(dimensions[0]/len(algorithm.array))
+    display.fill(pygame.Color("black"))
+    pygame.display.set_caption(
+        "Sorting Visualiser     Algorithm: {}     Time: {:.3f}      Status: Sorting".format(algorithm.name,
+                                                                                            time.time() - algorithm.start_time))
+    k = int(dimensions[0] / len(algorithm.array))
     for i in range(len(algorithm.array)):
-        colour = (0,0,0)
+        colour = "white"
         if swap1 == algorithm.array[i]:
-            colour = (0,255,0)
+           colour = "green"
         elif swap2 == algorithm.array[i]:
-            colour = (255,0,0)
-        pygame.draw.rect(display, colour, (i*k,dimensions[1],k,-algorithm.array[i]))
-    #play_sound(swap2) # - Uncomment if you want sound to play with each swap
+            colour = "red"
+        pygame.draw.rect(display, colour, (i * k, dimensions[1] - algorithm.array[i], k, algorithm.array[i]))
+    # play_sound(swap2) # - Uncomment if you want sound to play with each swap
     check_events()
     pygame.display.update()
 
+
 def keep_open(algorithm, display, time):
-    pygame.display.set_caption("Sorting Visiualiser     Algorithm: {}     Time: {:.3f}      Status: Done".format(algorithm.name, time))
+    pygame.display.set_caption(
+        "Sorting Visiualiser     Algorithm: {}     Time: {:.3f}      Status: Done".format(algorithm.name, time))
     while True:
         check_events()
         pygame.display.update()
+
 
 def main():
     if len(sys.argv) < 2:
@@ -99,6 +105,7 @@ def main():
         except:
             print("Error: {} is not a valid sorting algorithm".format(sys.argv[1]))
             print("Note: Sorting algorithms are in Camel Case")
+
 
 if __name__ == "__main__":
     main()
