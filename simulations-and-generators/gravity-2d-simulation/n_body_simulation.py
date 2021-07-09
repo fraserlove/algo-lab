cc8922ed13b6152377e631df1814ccbec5490faf
fraserlove <me@fraser.love>
2021-07-09 01:29:32 +0100

    Fixed bugs in gravity_2d_simulation_4.0.py and renamed to n_body_simulation.py

--- simulations-and-generators/gravity-2d-simulation/n_body_simulation.py
+++ simulations-and-generators/gravity-2d-simulation/n_body_simulation.py
@@ -1,45 +1,32 @@
-"""
-Version 4.0 of Gravity Simulation and Galaxy Formation
+'''
+N-body simulation
+Author: Fraser Love, me@fraser.love
+Created: 2021-07-06
+Latest Release: v1.0.2, 2021-07-08
+Python: v3.9.6
+Dependencies: pygame
 
-A Python 2D Simulation of Gravity
-Developed by Fraser Love on 06/07/21
-Dependencies: Pygame
-Ran on CPU - Recommended to use on High-Range CPU
-
-Simulating the force of gravity on celestial bodies and galaxy formation using Newtons laws and conservation of momentum.
-
-This version of the program is designed to be as accurate as possible and therefore is not meant to be ran in real-time.
-Instead the program saves an image of the simulation after each step, these images can then be combined to create a video
-file to show the whole simulation at the desired frame-rate.
-
-Every frame:
-- Calculate the weight of each object on every other object
-- Resolve the weight vector into x and y magnitudes
-- Update each objects velocity and position with the time_step
-- Check if particles have collided and if so use conservation of momentum to update new particles mass and velocity
-- Draw new objects on screen
-"""
+Simulating how N-bodies interact using gravitation as described newtonian physics. This program requires a huge amount of
+computation and therefore cannot be ran in real-time. Instead images are saved at each time-step in the simulation, these
+images can then be combined to create a video of the simulation at the desired frame-rate. It is reccomended to run this
+simulation on a powerful CPU to speed up computation.
+'''
 
 import random, sys, math, time, os
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 import pygame
 
-initial_objects = 2000   # Number of initial objects in the simulation
+initial_objects = 1000   # Number of initial objects in the simulation
 object_list = []  # List to hold all of the MassObjects in the simulation
 G = 6.67408e-11  # Gravitational constant
-time_step = 5000  # How much time has passed between every calculation - (lower more accurate: recommend < 10000)
+time_step = 2500  # How much time has passed between every calculation - (lower more accurate: recommend < 10000)
 max_v = 100  # A maximum velocity for objects to minimise inaccurate integrals of velocity
 dimensions = (1920, 1080)  # Dimensions of the simulation
 mass_to_radius_factor = 10  # Amount to which the radius should be scaled down by according to its weight
 max_initial_velocity = 10  # Maximum initial velocity of objects
 max_initial_mass = 40  # Maximum starting mass of objects
 min_initial_mass = 10  # Minimum starting mass of objects
-
-start_time = time.time()
-
-pygame.init()
-display = pygame.display.set_mode(dimensions)
-display.fill(pygame.Color("black"))
+collisions = False  # Enables or disables particle collisions
 
 
 class MassObject:
@@ -57,40 +44,43 @@ def setup(object_list):
         object_list.append(MassObject(random.randrange(min_initial_mass, max_initial_mass), random.randrange(1, dimensions[0]), random.randrange(1, dimensions[1]), random.randrange(-max_initial_velocity, max_initial_velocity) / 100000, random.randrange(-max_initial_velocity, max_initial_velocity) / 100000))
 
 
-def add_force(object):
-    x_acc = y_acc = 0.0
+def acceleration(object):
+    a, ax, ay = 0, 0, 0
     for other in object_list:
         if other != object:
-            r = ((other.x_pos - object.x_pos)**2) + ((other.y_pos - object.y_pos)**2)
-            fg = G * object.mass * other.mass / r
-            angle = math.atan2((other.y_pos - object.y_pos), (other.x_pos - object.x_pos))
-            x_acc += fg * math.cos(angle) / object.mass
-            y_acc += fg * math.sin(angle) / object.mass
-    return x_acc, y_acc
+            dx = other.x_pos - object.x_pos
+            dy = other.y_pos - object.y_pos
+            r_sqr = (dx * dx) + (dy * dy)
+            if r_sqr != 0:
+                a = G * other.mass / r_sqr
+            angle = math.atan2(dy, dx)
+            ax += a * math.cos(angle)
+            ay += a * math.sin(angle)
+    return ax, ay
 
 
 def update_pos(object_list):
     for object in object_list:
-        x_acc, y_acc = add_force(object)
-        object.x_velocity += x_acc * time_step
+        ax, ay = acceleration(object)
+        object.x_velocity += ax * time_step
         object.x_pos += object.x_velocity * time_step
-        object.y_velocity += y_acc * time_step
+        object.y_velocity += ay * time_step
         object.y_pos += object.y_velocity * time_step
 
 
 def check_collisions(object_list):
     max_mass = 0
     for object in object_list:
-        for other in object_list:
-            if object != other and object.mass > other.mass:
-                collision_radius = object.radius / 2
-                if (collision_radius > abs(other.x_pos - object.x_pos)) and (collision_radius > abs(other.y_pos - object.y_pos)):
-                    object.x_velocity = (object.x_velocity * object.mass + other.x_velocity * other.mass) / (object.mass + other.mass)
-                    object.y_velocity = (object.y_velocity * object.mass + other.y_velocity * other.mass) / (object.mass + other.mass)
-                    object.mass += other.mass
-                    object.radius = (object.mass / mass_to_radius_factor) ** (1/3)
-                    object_list.remove(other)
-
+        if collisions:
+            for other in object_list:
+                if object != other and object.mass > other.mass:
+                    collision_radius = object.radius
+                    if (collision_radius > abs(other.x_pos - object.x_pos)) and (collision_radius > abs(other.y_pos - object.y_pos)):
+                        object.x_velocity = (object.x_velocity * object.mass + other.x_velocity * other.mass) / (object.mass + other.mass)
+                        object.y_velocity = (object.y_velocity * object.mass + other.y_velocity * other.mass) / (object.mass + other.mass)
+                        object.mass += other.mass
+                        object.radius = (object.mass / mass_to_radius_factor) ** (1/3)
+                        object_list.remove(other)
         if object.mass >= max_mass:
             max_mass = object.mass
     return max_mass
@@ -105,6 +95,9 @@ def draw(object_list, max_mass):
 
 
 def main():
+    start_time = time.time()
+    pygame.init()
+    display = pygame.display.set_mode(dimensions)
     setup(object_list)
     step = 1
     os.makedirs("images/", exist_ok=True)
@@ -120,4 +113,5 @@ def main():
         step += 1
 
 
-main()
+if __name__ == '__main__':
+    main()
